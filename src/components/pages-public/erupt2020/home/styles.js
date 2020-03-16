1a1fd9f64b5d0d1d0661ef474883d9fed0778987
Alex Beaman <alex@insidenewcity.com>
2020-03-16 11:59:04 +0200

    refactor: remove unused styles

--- src/components/pages-public/erupt2020/home/styles.js
+++ src/components/pages-public/erupt2020/home/styles.js
@@ -13,19 +13,19 @@ const styles = (theme) => ({
         float: "right",
         paddingLeft: "10px",
     },
-    handSignalsImg: {
-        [theme.breakpoints.only("xs")]: {
-            maxWidth: "100%",
-            width: "100%",
-            paddingBottom: "15px",
-            paddingLeft: "0px",
-        },
-        maxWidth: "450px",
-        minWidth: "350px",
-        width: "40%",
-        float: "right",
-        paddingLeft: "10px",
-    },
+    // handSignalsImg: {
+    //     [theme.breakpoints.only("xs")]: {
+    //         maxWidth: "100%",
+    //         width: "100%",
+    //         paddingBottom: "15px",
+    //         paddingLeft: "0px",
+    //     },
+    //     maxWidth: "450px",
+    //     minWidth: "350px",
+    //     width: "40%",
+    //     float: "right",
+    //     paddingLeft: "10px",
+    // },
 });
 
 export default styles;
